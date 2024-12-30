2d1012489a7ab766a1f6060a797ff9cc86ffea5f
CyberFutureCloud <kokosk399@gmail.com>
2024-12-30 12:09:37 +0100

    Update cyberguardian_bot.py

--- cyberguardian_bot.py
+++ cyberguardian_bot.py
@@ -1,7 +1,13 @@
 import os
 import subprocess
-import git
 from datetime import datetime
+
+# Spróbuj zaimportować moduł git, jeśli jest zainstalowany
+try:
+    import git
+except ImportError:
+    git = None
+    print("Moduł 'git' nie jest zainstalowany. Użyj 'pip install gitpython', aby go zainstalować.")
 
 # Define bot configurations
 BOTS = [
@@ -25,12 +31,13 @@ def info():
     print("----------------------\n")
 
 def update():
+    if not git:
+        print("Operacja aktualizacji wymaga modułu 'git'. Zainstaluj go za pomocą 'pip install gitpython'.")
+        return
+
     try:
         repo_url = "https://github.com/CyberFutureCloud/CYBERBOT-GUARD.git"
         local_path = os.path.dirname(os.path.abspath(__file__))
-
-        if not os.path.exists(local_path):
-            os.makedirs(local_path)
 
         if os.path.exists(os.path.join(local_path, ".git")):
             repo = git.Repo(local_path)
